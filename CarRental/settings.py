bcdc8cdc37eaa45941befeb9cbf731b6ad0239ad
7Sumant <07sumantlokhande10@gmail.com>
2024-07-02 23:12:13 +0530

    Updated vercel.json and build script

--- CarRental/settings.py
+++ CarRental/settings.py
@@ -136,11 +136,12 @@ MESSAGE_TAGS = {
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'CarRental/static/'),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
